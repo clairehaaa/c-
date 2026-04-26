0f84540658cf56a0b17cfb251ea8dba52201093e
clairehaaa <syhaha06@gmail.com>
2026-04-26 15:23:32 -0500

    Create findMax.cpp

---
+++ c++/findMax.cpp
@@ -0,0 +1,23 @@
+//find maximum value between two numbers using function
+#include <iostream>
+using namespace std;
+
+int findMax(int,int);
+int main()
+{
+    int tmp;
+    int n1,n2;
+    cin >> n1 >> n2;
+    cout << "Find maximum: ";
+    tmp = findMax(n1,n2);
+    cout << tmp << endl;
+    return 0;
+}
+
+int findMax(int n, int m)
+{
+    int tmp;
+    if(m>=n) tmp = m;
+    else tmp = n;
+    return tmp;
+}
